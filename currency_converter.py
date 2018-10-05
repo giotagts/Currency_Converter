ab9db10f621ebc79711f67fa9c5d1a26e792d656
giotagts <giotagts@yahoo.gr>
2018-10-05 17:51:32 +0300

    amended function

--- currency_converter.py
+++ currency_converter.py
@@ -1,6 +1,7 @@
 import argparse
 from converter import conv
-from free_currApi import symb
+from free_currApi import code2symb
+from free_currApi import symb2code
 from json_api import r
 from json_api import base
 import json
@@ -15,19 +16,25 @@ parser.add_argument('--output_currency', help='a valid 3 letter code',required=F
 args = parser.parse_args()
 #print args
 
+if len(args.input_currency)!=3:
+    args.input_currency=symb2code(args.input_currency)
+
+
 if(str(args.input_currency) != base):
     in_cr=r.json()['quotes'][base+''+str(args.input_currency)]
 else:
     in_cr=args.amount
 
 
+
 if args.output_currency:
     out_cr=r.json()['quotes'][base+''+str(args.output_currency)]
-    #print ''+str(args.amount)+' '+str(args.input_currency)+' = '+str(args.output_currency)
     out_am=conv(float(in_cr),float(out_cr),args.amount)
-    print '' + str(symb(str(args.input_currency)))+''+ str(args.amount) +' ('+str(args.input_currency)+') = '+ str(symb(str(args.output_currency)))+''+str(out_am) +' (' + str(args.output_currency)+')'
+    print '' + str(code2symb(str(args.input_currency)))+''+ str(args.amount) +' ('+str(args.input_currency)+') = '+ str(code2symb(str(args.output_currency)))+''+str(out_am) +' (' + str(args.output_currency)+')'
 else:
     for i in r.json()['quotes']:
         out_am=conv(float(in_cr),float(r.json()['quotes'][i]),args.amount)
         code=i[3:]
-        print '' + str(symb(str(args.input_currency)))+''+ str(args.amount) +' ('+str(args.input_currency)+') = '+ str(out_am) +' (' + str(code)+ ')'
+        print '' + str(code2symb(str(args.input_currency)))+''+ str(args.amount) +' ('+str(args.input_currency)+') = '+ str(out_am) +' (' + str(code)+ ')'
+
+
--- free_currApi.py
+++ free_currApi.py
@@ -3,10 +3,16 @@
 import requests
 countries=requests.get('https://free.currencyconverterapi.com/api/v6/countries')
 
-def symb(b):
+def code2symb(b):
     for k in countries.json()['results']:
         if countries.json()['results'][k]['currencyId']==b:
             symbol=countries.json()['results'][k]['currencySymbol']
             return symbol
             break
-    
+
+def symb2code(d):
+    for l in countries.json()['results']:
+        if countries.json()['results'][l]['currencySymbol']==d:
+            code3=countries.json()['results'][l]['currencyId']
+            return code3
+            break
